95eccf37264f348195ee2b608ffdb55196a2325e
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-03-24 13:43:02 +0200

    add toolTiper

--- src/js/script.js
+++ src/js/script.js
@@ -233,4 +233,24 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
     };
     popup();
 
+    // Подсказки
+    $(document).ready(function(){
+        toolTiper();
+    });
+      
+    function toolTiper(effect) {
+      $('.tooltiper').each(function(){
+        var eLcontent = $(this).attr('data-tooltip'),
+            eLtop = $(this).position().top,
+            eLleft = $(this).position().left;
+        $(this).append('<span class="tooltip">'+eLcontent+'</span>');
+        var eLtw = $(this).find('.tooltip').width(),
+            eLth = $(this).find('.tooltip').height();
+        $(this).find('.tooltip').css({
+          "top": (0 - eLth - 40)+'px',
+          "left":'-10px'
+        });
+      });
+    }
+
 });
